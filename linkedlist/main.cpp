ebba9ae7df95abac710870c98f52b0e4ab0825f5
Sadiq <53291095+sadiqshaik123@users.noreply.github.com>
2024-01-27 17:38:54 +0530

    Update main.cpp

--- linkedlist/main.cpp
+++ linkedlist/main.cpp
@@ -4,60 +4,51 @@ using namespace std;
 struct Node{
     int data;
     Node *next;
-    Node(int value):data(value),next(nullptr){}
-
+    Node(int value){
+        data=value;
+        next=nullptr;
+    }
 };
 class LinkedList{
 public:
-    Node*head;
-    LinkedList():head(nullptr){}
+    Node *head;
+    LinkedList(){
+        head=nullptr;
+    }
+    
     void insert(int value){
-        Node *newNode=new Node(value);
+        Node *NewNode=new Node(value);
         if(!head){
-            head=newNode;
+            head=NewNode;
         }
         else{
-            Node*temp=head;
+            Node *temp=head;
             while(temp->next){
                 temp=temp->next;
             }
-            temp->next =newNode;
+            temp->next=NewNode;
         }
-
     }
     void display(){
-        Node*temp=head;
+        Node *temp=head;
         while(temp){
             cout<<temp->data<<"->";
             temp=temp->next;
         }
         cout<<"nullptr"<<endl;
     }
-
     void deleteNode(int value){
-        Node *temp=head;
-        if(temp && temp->data == value){
-            head=temp->next;
-            delete temp;
-            cout << "Element " << value << " deleted successfully." << endl;
-            return;
-        }
-        while(temp && temp->data&& temp->next->data != value){
+        Node* temp=head;
+        while(temp->next->data!=value){
             temp=temp->next;
         }
-        if (temp && temp->next) {
-            Node *deleteNode = temp->next;
-            temp->next = temp->next->next;
-            delete deleteNode;
-            cout << "Element " << value << " deleted successfully." << endl;
-        }
-        else{
-            cout<<"element not found"<<endl;
-        }
+        Node *deleteele=temp->next;
+        temp->next=temp->next->next;
+        delete deleteele;
+        cout<<"element deleted"<<endl;
     }
-
+    
 };
-
 int main()
 {
     LinkedList myList;
@@ -88,7 +79,5 @@ int main()
         }
 
     }
-
-
     return 0;
 }
